8edb077844e01b7f635289260f38f9d544fe4728
Sprite <noreply@sprites.dev>
2026-05-16 22:51:00 +0000

    fix: correct OriginRequestPolicy name ALL_VIEWER_AND_CLOUDFRONT_2022

--- cdk/lib/constructs/cloudfront.ts
+++ cdk/lib/constructs/cloudfront.ts
@@ -31,7 +31,7 @@ export class CloudFront extends Construct {
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
         cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_AND_CLOUDFRONT_HEADERS,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_AND_CLOUDFRONT_2022,
       },
       additionalBehaviors: {
         '/static/*': {
